a14c81dc4ab1fe0fa2f6d437c51aba2eb37b8431
Mahesh K <2036998+mkopnsrc@users.noreply.github.com>
2024-02-10 01:58:47 -0500

    Create install-vscode-server.sh
    initial install vscode server script
    moved from local to public git

---
+++ install-vscode-server.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Function to detect OS type
+detect_os() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "darwin"
+    else
+        echo "Unsupported OS"
+        exit 1
+    fi
+}
+
+# Function to detect architecture
+detect_architecture() {
+    architecture=$(uname -m)
+    case $architecture in
+        x86_64)
+            echo "x64"
+            ;;
+        *)
+            echo "Unsupported architecture"
+            exit 1
+            ;;
+    esac
+}
+
+# Function to download latest version of VS Code Server
+download_vscode_server() {
+    local os=$1
+    local arch=$2
+    local latest_url="https://update.code.visualstudio.com/commit:${os}_${arch}/server-linux-${arch}.tar.gz"
+    echo "Downloading latest Visual Studio Code Server for $os $arch..."
+    curl -fL $latest_url -o vscode_server.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "Failed to download Visual Studio Code Server."
+        exit 1
+    fi
+    echo "Visual Studio Code Server downloaded successfully."
+}
+
+# Main function
+main() {
+    local os=$(detect_os)
+    local arch=$(detect_architecture)
+    download_vscode_server $os $arch
+}
+
+# Execute main function
+main
